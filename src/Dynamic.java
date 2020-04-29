baa4c020700df521b3cfc56797ec669af9dc2b1d
Jianqi <a1752399@student.adelaide.edu.au>
2020-04-29 15:47:40 +0930

    Add the dynamic programming for Number of Coin

--- src/Dynamic.java
+++ src/Dynamic.java
@@ -1,7 +1,13 @@
 public class Dynamic {
     public static void main(String[] args) {
-
+        System.out.println(numberOfCoin(4));
     }
+
+    /**
+     * Author: Answer from LeetCode
+     * This question is not a normal Dynamic Problem
+     * The url of this question is "https://leetcode.com/problems/regular-expression-matching/"
+     * */
 
     enum Result{
         TRUE,
@@ -33,5 +39,34 @@ public class Dynamic {
         }
         memo[i][j] = ans ? Result.TRUE : Result.FALSE;
         return ans;
+    }
+
+    /**
+     * This is quiet easy question for dynamic programming
+     * This one is to calculate the number of coins for certain value
+     * */
+    public static int numberOfCoin(int num){
+        /**
+         * There are three types of coin (1,3,5)
+         * */
+        int[] result = new int[num + 1];
+
+        if(num == 0){
+            return result[0];
+        }
+
+        for (int i = 1; i < result.length; i++) {
+            if(i < 3 && result[i] == 0){
+                result[i] = result[i - 1] + 1;
+            }
+            if(i >= 3 && i < 5 && result[i] == 0){
+                result[i] = Math.min(result[i - 1] + 1, result[i - 3] + 1);
+            }
+            if(i >=5 && result[i] == 0){
+                result[i] = Math.min(result[i - 1]+ 1, Math.min(result[i - 3] + 1, result[i - 5] + 1));
+            }
+        }
+
+        return result[num];
     }
 }
